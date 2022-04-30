da086e4bd4e0a755f9d7c8f0159e7d9a7c9f1d8f
XAYRGA <dzbush25@gmail.com>
2022-04-30 10:45:18 -0400

    add checksum computation

--- tx8tool/Program.cs
+++ tx8tool/Program.cs
@@ -1,9 +1,13 @@
 using System;
 using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
 using xayrga;
 
 internal static class Program
 {
+
 	private static void Main(string[] args)
 	{
 
@@ -33,31 +37,31 @@ internal static class Program
 		{
 			switch (operation)
 			{
-			case "encode":
-				inFilePath = cmdarg.assertArg(1, "File Name");
-				outFilePath = cmdarg.assertArg(2, "Output File");
-				Encode(inFilePath, outFilePath);
-				break;
-			case "decode":
-				inFilePath = cmdarg.assertArg(1, "File Name");
-				outFilePath = cmdarg.assertArg(2, "Output File");
-				Decode(inFilePath, outFilePath);
-				break;
-			default:
-				Console.WriteLine("tx8tool by Xayrga\ntx8tool.exe <file>\t Converts a .DDS into a TX8 or TX8 into a DDS\n\tencode <file> <output file>\n\tdecode <file> <output file>");
-				break;
+				case "encode":
+					inFilePath = cmdarg.assertArg(1, "File Name");
+					outFilePath = cmdarg.assertArg(2, "Output File");
+					Encode(inFilePath, outFilePath);
+					break;
+				case "decode":
+					inFilePath = cmdarg.assertArg(1, "File Name");
+					outFilePath = cmdarg.assertArg(2, "Output File");
+					Decode(inFilePath, outFilePath);
+					break;
+				default:
+					Console.WriteLine("tx8tool by Xayrga\ntx8tool.exe <file>\t Converts a .DDS into a TX8 or TX8 into a DDS\n\tencode <file> <output file>\n\tdecode <file> <output file>");
+					break;
 			}
 		}
 		else
 		{
 			switch (opFlag)
 			{
-			case 1:
-				Decode(inFilePath, outFilePath);
-				break;
-			case 2:
-				Encode(inFilePath, outFilePath);
-				break;
+				case 1:
+					Decode(inFilePath, outFilePath);
+					break;
+				case 2:
+					Encode(inFilePath, outFilePath);
+					break;
 			}
 		}
 	}
@@ -65,26 +69,37 @@ internal static class Program
 	private static void Encode(string inF, string outF)
 	{
 		byte[] bufferFile = File.ReadAllBytes(inF);
+		byte[] bufferFullFile = new byte[bufferFile.Length + 0x20];
+
 		for (int i = 0; i < bufferFile.Length; i++)
-		{
-			unchecked
-			{
-				bufferFile[i] += 0xA0;
-			}
-		}
-		File.WriteAllBytes(outF, bufferFile);
+			bufferFullFile[i] = bufferFile[i];
+
+
+		var md5 = MD5.Create();
+		var bx = md5.ComputeHash(bufferFile);
+		var hash = Encoding.ASCII.GetBytes(BitConverter.ToString(bx).Replace("-", ""));
+
+		for (int i = 0; i < hash.Length; i++)
+			bufferFullFile[(bufferFile.Length) + i] = hash[i];
+
+		for (int i = 0; i < bufferFullFile.Length; i++)
+			unchecked {bufferFullFile[i] -= 96;}
+	
+
+		File.WriteAllBytes(outF, bufferFullFile);	
 	}
 
 	private static void Decode(string inF, string outF)
 	{
 		byte[] bufferFile = File.ReadAllBytes(inF);
-		for (int i = 0; i < bufferFile.Length; i++)
+		byte[] file = new byte[bufferFile.Length - 0x20];
+		for (int i = 0; i < bufferFile.Length - 0x20; i++)
 		{
 			unchecked
 			{
-				bufferFile[i] -= 0xA0;
+				file[i] = (bufferFile[i] += 96);
 			}
 		}
-		File.WriteAllBytes(outF, bufferFile);
+		File.WriteAllBytes(outF, file);
 	}
 }
